e6eee36ba826838f78703522d60fe639b05ac068
elisa-morales <morales.elisa@yahoo.it>
2023-03-25 12:02:27 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import generateJoke from "./generateJoke"
+import axios from "axios"
+import _ from "lodash"
+import "./styles/main.scss"
+
+const jokeBtn = document.getElementById("jokeBtn")
+jokeBtn.addEventListener("click", generateJoke)
+
+generateJoke()
+
+let count = 0
+let newsId = ""
+
+async function fecthAllNews() {
+  await axios
+    .get("https://hacker-news.firebaseio.com/v0/newstories.json")
+    .then((res) => {
+      const newsData = _.get(res, "data", "")
+      newsId = newsData.slice(count, count + 10).forEach(fetchNewsId)
+      console.log("bu")
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+    .finally(() => {})
+}
+
+async function fetchNewsId(id) {
+  await axios
+    .get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+    .then((res) => {
+      const itemData = _.get(res, "data", "")
+      console.log(itemData)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+    .finally(() => {})
+}
+
+fecthAllNews()
