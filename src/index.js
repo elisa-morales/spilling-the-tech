3f573117be8efd191fc8c620b9f064c75e9d4517
elisa-morales <morales.elisa@yahoo.it>
2023-03-28 21:31:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,25 @@ import _ from "lodash"
 import "./styles/main.scss"
 
 let count = 0
-let newsId = ""
-let newsDate = ""
+let newsDay = ""
 let newsTime = ""
+const baseUrl = "https://hacker-news.firebaseio.com/v0/"
 const loader = document.getElementById("loading")
 const loadMoreBtn = document.getElementById("load-more-btn")
 const footer = document.getElementById("footer-content")
 
-async function fecthAllNews() {
+loadMoreBtn.addEventListener("click", loadMore)
+
+async function fetchAllNews() {
   await axios
-    .get("https://hacker-news.firebaseio.com/v0/newstories.json")
+    .get(`${baseUrl}newstories.json`)
     .then((res) => {
-      const newsData = _.get(res, "data", "")
-      newsId = newsData.slice(count, count + 10).forEach(fetchNewsId)
+      const newsData = _.get(res, "data")
+      newsData.slice(count, count + 10).forEach(fetchNewsId)
+
+      if (newsData === 0) {
+        loadMoreBtn.disabled = true
+      }
     })
     .catch((err) => {
       console.log(err)
@@ -26,26 +32,27 @@ async function fecthAllNews() {
 async function fetchNewsId(id) {
   loader.classList.add("display")
   await axios
-    .get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+    .get(`${baseUrl}item/${id}.json`)
     .then((res) => {
-      const itemData = _.get(res, "data", "")
+      const itemData = _.get(res, "data")
+
       getDate(itemData)
       renderNews(itemData)
-      footer.style.visibility = "visible"
-      loadMoreBtn.style.visibility = "visible"
     })
     .catch((err) => {
       console.log(err)
     })
     .finally(() => {
       loader.classList.remove("display")
+      footer.style.visibility = "visible"
+      loadMoreBtn.style.visibility = "visible"
     })
 }
 
 function getDate(data) {
   const unixTimestamp = data.time
   const date = new Date(unixTimestamp * 1000)
-  newsDate = date.toLocaleDateString("default")
+  newsDay = date.toLocaleDateString("default")
   newsTime = date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
 }
 
@@ -53,9 +60,15 @@ function renderNews(data) {
   document.getElementById("news-wrapper").innerHTML += `
   <div id="container">
           <h2>${data.title}</h2>
-          <p>Posted by ${data.by} | ⌚ ${(newsDate, newsTime)}</p>
+          <p>Posted by ${data.by} | ⌚ ${newsDay}, ${newsTime}</p>
           <button><a href="${data.url}" target="_blank">Read article</a></button>
         </div>`
 }
 
-fecthAllNews()
+function loadMore() {
+  count += 10
+  fetchAllNews()
+  renderNews()
+}
+
+fetchAllNews()
