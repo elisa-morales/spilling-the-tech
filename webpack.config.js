c60fcfa26ef4f95cb2cc92b93d4ab4b814ff2c32
elisa-morales <morales.elisa@yahoo.it>
2023-03-29 10:52:20 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path")
 const Dotenv = require("dotenv-webpack")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin
 
 module.exports = {
   mode: "development",
@@ -44,7 +43,6 @@ module.exports = {
       filename: "index.html",
       template: "src/template.html",
     }),
-    new BundleAnalyzerPlugin(),
     new Dotenv({
       systemvars: true,
       silent: true,
