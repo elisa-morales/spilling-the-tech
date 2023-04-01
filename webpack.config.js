4c16acd507ba6bfb055968f6270e4ba6863aac3f
elisa-morales <morales.elisa@yahoo.it>
2023-04-01 07:56:41 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.js$/,
